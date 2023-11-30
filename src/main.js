8372a5e16ee20234b27acadb94fd8b7470a43cda
111522078 <yalanna0.0@gmail.com>
2023-11-30 20:31:58 +0800

    add: GA

--- src/main.js
+++ src/main.js
@@ -6,10 +6,19 @@ import homepage from './components/homepage.vue'
 import content from './components/content.vue'
 import './assets/global.css'; // 或 './styles/global.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import VueGtag from 'vue-gtag'
 
 const routes = [
-    { path: '/', component: homepage },
-    { path: `/${process.env.VUE_APP_PAPER_ID}-:subId`, component: content },
+    { 
+        path: '/', 
+        component: homepage, 
+        meta: { title: '中央大學電子報'} 
+    },
+    { 
+        path: `/${process.env.VUE_APP_PAPER_ID}-:subId`, 
+        component: content, 
+        meta: { title: (route) => `中大客家學院電子報第 ${process.env.VUE_APP_PAPER_ID} 期第 ${route.params.subId} 篇`} 
+    },
 ]
 
 
@@ -24,8 +33,19 @@ const router = createRouter({
     }
 });
 
+router.afterEach((to) => {
+    // 使用路由的meta欄位設置標題
+    const title = typeof to.meta.title === 'function' ? to.meta.title(to) : to.meta.title;
+    document.title = title || '中央大學電子報';
+});
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
 app.mount('#app');
+
+app.use(VueGtag, {
+    config: {
+        id: process.env.VUE_APP_GA_ID
+    }
+})
